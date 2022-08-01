8b7a5a9dfc59bfe8fe6f10b592b121bd8a6715b4
NicMortelliti <npmortelliti@gmail.com>
2022-08-01 11:41:20 -0700

    Add rescue response

--- app/controllers/spices_controller.rb
+++ app/controllers/spices_controller.rb
@@ -1,2 +1,11 @@
 class SpicesController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+# Private methods
+private 
+
+def render_not_found_response
+  renders json: { error: 'Spice not found' }, status: :not_found
+end
+
 end
