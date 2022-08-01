ed6a1fae0a33ae14c58db0ad67fba8d14449590c
NicMortelliti <npmortelliti@gmail.com>
2022-08-01 11:54:32 -0700

    Add Post controller

--- app/controllers/spices_controller.rb
+++ app/controllers/spices_controller.rb
@@ -1,25 +1,32 @@
 class SpicesController < ApplicationController
 rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-# GET /spices
-def index
-  spices = Spice.all
-  render json: spices
-end
+  # GET /spices
+  def index
+    spices = Spice.all
+    render json: spices
+  end
 
-# POST /spices
+  # POST /spices
+  def create
+    spice = Spice.create(spice_params)
+    render json: spice, status: :created
+  end
+
+  # PATCH /spices/:id
 
 
-# PATCH /spices/:id
+  # DELETE /spices/:id
 
+  # Private methods
+  private 
 
-# DELETE /spices/:id
+  def spice_params
+    params.permit(:title, :image, :description, :notes, :rating)
+  end
 
-# Private methods
-private 
-
-def render_not_found_response
-  renders json: { error: 'Spice not found' }, status: :not_found
-end
+  def render_not_found_response
+    renders json: { error: 'Spice not found' }, status: :not_found
+  end
 
 end
